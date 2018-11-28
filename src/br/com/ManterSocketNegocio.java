e941705f447cafa98020175de0d73820e954c5fc
willbigas <williambmauro@hotmail.com>
2018-11-28 11:37:55 -0200

    Adicionado Implementacao de Socket - 20181128 - 11:37

---
+++ src/br/com/ManterSocketNegocio.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com;
+
+import br.com.kahoot.entidade.Disciplina;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author William
+ */
+public class ManterSocketNegocio {
+
+    public static Object recebendoDadosViaSocket(Integer porta) throws IOException, ClassNotFoundException, InterruptedException {
+        ServerSocket servidor = new ServerSocket(porta);
+        Socket entradaDados = servidor.accept();
+        ObjectInputStream input = new ObjectInputStream(entradaDados.getInputStream());
+        Object objRecebido = input.readObject();
+        servidor.close();
+        entradaDados.close();
+        return objRecebido;
+    }
+
+    public static void enviandoDadosViaSocket(String enderecoIp, Integer porta, Object objParaEnviar) throws Exception {
+        InetAddress endereco = InetAddress.getByName(enderecoIp);
+        Socket saidaDados = new Socket(endereco, porta);
+        ObjectOutputStream saida = new ObjectOutputStream(saidaDados.getOutputStream()); // saida de objeto da classe cliente
+        saida.flush();
+        saida.writeObject(objParaEnviar);
+        saida.close();
+        saidaDados.close(); // fechando acesso ao servidor
+
+    }
+}
--- src/br/com/PrincipalCliente.java
+++ src/br/com/PrincipalCliente.java
@@ -1,9 +1,10 @@
 package br.com;
 
+import br.com.kahoot.entidade.Assunto;
 import br.com.kahoot.entidade.Disciplina;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -14,21 +15,37 @@ public class PrincipalCliente {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-
-        Socket cliente = new Socket("localhost", 8787);
-
+    public static void main(String[] args) throws Exception {
         Disciplina disciplina = new Disciplina();
         disciplina.setId(1);
-        disciplina.setNome("William");
+        disciplina.setNome("POO");
         disciplina.setProfessor("Felipe");
+        
+        Assunto assunto = new Assunto();
+        assunto.setDisciplina(disciplina);
+        assunto.setId(1);
+        assunto.setNome("Encapsulamento");
+        Object objDisciplina = disciplina;
+        Object ObjAssunto = assunto;
+        List<Object> OBJECTS = new ArrayList<>();
+        OBJECTS.add(ObjAssunto);
+        OBJECTS.add(objDisciplina);
 
-        ObjectOutputStream output = new ObjectOutputStream(cliente.getOutputStream()); // saida de objeto da classe cliente
-        output.flush();
-        output.writeObject(disciplina);
-        output.reset();
-        output.close();
-        cliente.close(); // fechando acesso ao servidor
+        ManterSocketNegocio.enviandoDadosViaSocket("localhost", 8787, OBJECTS);
+
+//        Socket cliente = new Socket("localhost", 8787);
+//
+//        Disciplina disciplina = new Disciplina();
+//        disciplina.setId(1);
+//        disciplina.setNome("William");
+//        disciplina.setProfessor("Felipe");
+//
+//        ObjectOutputStream output = new ObjectOutputStream(cliente.getOutputStream()); // saida de objeto da classe cliente
+//        output.flush();
+//        output.writeObject(disciplina);
+//        output.reset();
+//        output.close();
+//        cliente.close(); // fechando acesso ao servidor
     }
 
 }
