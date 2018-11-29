3e00007de8f05a277b7db425b2abb27b56803548
William Bigas Mauro <williambmauro@hotmail.com>
2018-11-29 20:16:10 -0200

    Adicionado swing

--- src/br/com/kahoot/principal/PrincipalCliente.java
+++ src/br/com/kahoot/principal/PrincipalCliente.java
@@ -1,13 +1,12 @@
 package br.com.kahoot.principal;
 
 import br.com.kahoot.negocio.ManterKahootNegocio;
-import br.com.kahoot.entidade.Disciplina;
 import br.com.kahoot.entidade.Pergunta;
-import br.com.kahoot.entidade.Resposta;
 import br.com.kahoot.entidade.Servidor;
-import br.com.kahoot.entidade.Usuario;
-import java.util.ArrayList;
+import br.com.view.FramePrincipal;
+import br.com.view.PanelJogar;
 import java.util.List;
+import javax.swing.JFrame;
 
 /**
  *
@@ -17,15 +16,23 @@ public class PrincipalCliente {
 
     public static Servidor CONFIGURACAO_GLOBAL = new Servidor();
 
+    private static FramePrincipal frame;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
 
+        frame = new FramePrincipal();
+        frame.setTitle("Quiz Senac!");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
         /**
          * Configuracao de Ip e porta Aqui!
          */
-        CONFIGURACAO_GLOBAL.setIp("localhost");
+        CONFIGURACAO_GLOBAL.setIp("192.168.109.46");
         CONFIGURACAO_GLOBAL.setPorta(8787);
 
         List<Pergunta> objetosRecebidosParaTeste = ManterKahootNegocio.recebendoPerguntasViaSocket();
@@ -34,6 +41,12 @@ public class PrincipalCliente {
             System.out.println(objetosRecebidosParaTeste.get(i));
 
         }
+    }
+
+    public static void panelUsuarioJogar() throws Exception {
+        PanelJogar panel = new PanelJogar();
+        frame.setContentPane(panel);
+        frame.setVisible(true);
     }
 
 }
